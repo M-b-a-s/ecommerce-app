4c1498ea7a5cb53611852991f3e9fbcb5a0d7b48
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-12-08 12:16:13 +0100

    initial commit

---
+++ src/test/java/com/mbas/ecommerce/app/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbas.ecommerce.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
